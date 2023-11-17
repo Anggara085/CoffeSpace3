c18645f7e8e56fde801d9c8d5095f8744fc74c41
Anggara085 <139990052+Anggara085@users.noreply.github.com>
2023-11-17 19:43:54 +0700

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,48 +1,55 @@
-import React, { Fragment } from 'react'
-import Menu from './Menu'
+import React, { Fragment } from "react";
+import Menu from "./Menu";
 
 function Login() {
   return (
     <Fragment>
-        <>
-  <main className="login-body" data-vide-bg="assets/img/BackgroundLogin.jpg">
-    {/* Login Admin */}
-    <form className="form-default" action="BackgroundLogin.jpg" method="POST">
-      <div className="login-form">
-        {/* logo-login */}
-        <div className="logo-login">
-          <a href="index.html">
-            <img src="assets/img/logo/loder.png" alt="" />
-          </a>
-        </div>
-        <h2>Login Here</h2>
-        <div className="form-input">
-          <label htmlFor="name">Email</label>
-          <input type="email" name="email" placeholder="Email" />
-        </div>
-        <div className="form-input">
-          <label htmlFor="name">Password</label>
-          <input type="password" name="password" placeholder="Password" />
-        </div>
-        <div className="col-2 mx-auto pt-30">
-        <a href="/Index2" className="btn btn3">
-          Masuk
-        </a>
-        </div>
-        {/* Forget Password */}
-        {/* <a href="#" class="forget">Forget Password</a> */}
-        {/* Forget Password */}
-        <a href="register.html" className="registration">
-          Registrasi
-        </a>
-      </div>
-    </form>
-    {/* /end login form */}
-  </main>
-</>
-
+      <>
+        <main
+          className="login-body"
+          style={{ backgroundImage: 'url("assets/img/BackgroundLogin.jpg")' }}
+        >
+          {/* Your login content goes here */}
+          {/* Login Admin */}
+          <form
+            className="form-default"
+            action="BackgroundLogin.jpg"
+            method="POST"
+          >
+            <div className="login-form">
+              {/* logo-login */}
+              <div className="logo-login">
+                <a href="index.html">
+                  <img src="assets/img/logo/loder.png" alt="" />
+                </a>
+              </div>
+              <h2>Login Here</h2>
+              <div className="form-input text-left">
+                <label htmlFor="name">Email</label>
+                <input type="email" name="email" placeholder="Email" />
+              </div>
+              <div className="form-input text-left">
+                <label htmlFor="name">Password</label>
+                <input type="password" name="password" placeholder="Password" />
+              </div>
+              <div className="col-2 mx-auto pt-30">
+                <a href="/Index2" className="btn btn3">
+                  Masuk
+                </a>
+              </div>
+              {/* Forget Password */}
+              {/* <a href="#" class="forget">Forget Password</a> */}
+              {/* Forget Password */}
+              <a href="/Gabung" className="registration text-left">
+                Registrasi
+              </a>
+            </div>
+          </form>
+          {/* /end login form */}
+        </main>
+      </>
     </Fragment>
-  )
+  );
 }
 
-export default Login
+export default Login;
